f3ebf74e1d251e0fa12920b1c9ed6c31e854d00b
wally <goldenale5500@gmail.com>
2023-10-24 02:38:52 +0900

    RestTemplate로 payment랑 통신

--- src/main/java/com/wally/ps/api/entity/Payment.java
+++ src/main/java/com/wally/ps/api/entity/Payment.java
@@ -12,4 +12,6 @@ public class Payment {
     private int paymentId;
     private String paymentStatus;
     private String transactionId;
+    private int orderId;
+    private double amount;
 }
--- src/main/java/com/wally/ps/api/service/PaymentService.java
+++ src/main/java/com/wally/ps/api/service/PaymentService.java
@@ -5,6 +5,7 @@ import com.wally.ps.api.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
 import java.util.UUID;
 
 @Service
@@ -14,8 +15,13 @@ public class PaymentService {
     private final PaymentRepository paymentRepository;
 
     public Payment doPayment(Payment payment) {
-
+        payment.setPaymentStatus(paymentProcessing());
         payment.setTransactionId(UUID.randomUUID().toString());
         return paymentRepository.save(payment);
+    }
+
+    // 무작위로 성공했는지 테스트용
+    public String paymentProcessing() {
+        return new Random().nextBoolean() ? "success" : "false";
     }
 }
